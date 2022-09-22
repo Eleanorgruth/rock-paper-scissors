7df68ddde3fb5db5c72efbf0b98833f84c59c49f
Eleanor Ruth <ellie.ruth@me.com>
2022-09-21 18:53:51 -0600

    [WIP] Player class

---
+++ player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor(playerName, playerToken) {
+    this.name = playerName
+    this.token = playerToken
+    this.wins = 0
+  }
+  takeTurn() {
+//what each player selects 
+  }
+}
